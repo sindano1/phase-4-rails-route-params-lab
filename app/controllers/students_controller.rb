339bc33195d4e88dd580df49353c8aedf2423834
Sarah Indano <sarah.indano@gmail.com>
2022-04-26 13:59:44 -0400

    commit tree ba715b4dd13594b534a19b0cf9bd4a929c97d344

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    specific_student = Student.find(params[:id])
+    render json: specific_student
+  end
+
 end
